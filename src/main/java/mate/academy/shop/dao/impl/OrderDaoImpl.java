6c74a5428779c6bdffcf1be51ab5d8f678e7d197
Rostislav Levadnyi <rlevadniy@ukr.net>
2020-05-14 13:45:20 +0300

    resoling troubles

---
+++ src/main/java/mate/academy/shop/dao/impl/OrderDaoImpl.java
@@ -0,0 +1,54 @@
+package mate.academy.shop.dao.impl;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import mate.academy.shop.dao.OrderDao;
+import mate.academy.shop.db.Storage;
+import mate.academy.shop.lib.Dao;
+import mate.academy.shop.model.Order;
+import mate.academy.shop.model.User;
+
+@Dao
+public class OrderDaoImpl implements OrderDao {
+    @Override
+    public Order create(Order order) {
+        Storage.addOrder(order);
+        return order;
+    }
+
+    @Override
+    public Optional<Order> get(Long id) {
+        return Storage.orders
+                .stream()
+                .filter(order -> order.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public List<Order> getUserOrders(User user) {
+        return Storage.orders
+                .stream()
+                .filter(order -> order.getUser().getId().equals(user.getId()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Order> getAll() {
+        return Storage.orders;
+    }
+
+    @Override
+    public Order update(Order order) {
+        IntStream.range(0, Storage.orders.size())
+                .filter(i -> Storage.orders.get(i).getId().equals(order.getId()))
+                .forEach(o -> Storage.orders.set(o, order));
+        return order;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return Storage.orders.removeIf(order -> order.getId().equals(id));
+    }
+}
---
+++ src/main/java/mate/academy/shop/dao/impl/ShoppingCardDaoImpl.java
@@ -0,0 +1,53 @@
+package mate.academy.shop.dao.impl;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.IntStream;
+import mate.academy.shop.dao.ShoppingCardDao;
+import mate.academy.shop.db.Storage;
+import mate.academy.shop.lib.Dao;
+import mate.academy.shop.model.ShoppingCard;
+
+@Dao
+public class ShoppingCardDaoImpl implements ShoppingCardDao {
+    @Override
+    public ShoppingCard create(ShoppingCard obj) {
+        Storage.addShoppingCard(obj);
+        return obj;
+    }
+
+    @Override
+    public Optional<ShoppingCard> get(Long id) {
+        return Storage.shoppingCards
+                .stream()
+                .filter(sh -> sh.getId().equals(id))
+                .findFirst();
+    }
+
+    @Override
+    public ShoppingCard getByUserId(Long userId) {
+        return getAll()
+                .stream()
+                .filter(sc -> sc.getUser().getId().equals(userId))
+                .findFirst()
+                .get();
+    }
+
+    @Override
+    public List<ShoppingCard> getAll() {
+        return Storage.shoppingCards;
+    }
+
+    @Override
+    public ShoppingCard update(ShoppingCard shoppingCard) {
+        IntStream.range(0, Storage.shoppingCards.size())
+                .filter(sh -> Storage.shoppingCards.get(sh).getId().equals(shoppingCard.getId()))
+                .forEach(s -> Storage.shoppingCards.set(s, shoppingCard));
+        return shoppingCard;
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        return Storage.shoppingCards.removeIf(sh -> sh.getId().equals(id));
+    }
+}
